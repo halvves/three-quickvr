4f6bc67fb43b590f8b5030cee1d7511a14bbcad5
halvves <hi@halvves.com>
2017-04-13 16:42:03 -0500

    quickquickquick

---
+++ README.md
@@ -0,0 +1,25 @@
+# Get started with VR in THREE.js... QUICK.
+
+[![Latest NPM release][npm-badge]][npm-badge-url]
+[![License][license-badge]][license-badge-url]
+[![Dependencies][dependencies-badge]][dependencies-badge-url]
+
+I didn't like any of the existing solutions I could find for using VRControls in a modular js build. So I made this.
+
+1. Install:
+```bash
+npm i -S three-quickvr
+```
+
+2. Use:
+```javascript
+import VRControls from 'three-quickvr';
+const controls = new VRControls(camera);
+```
+
+[npm-badge]: https://img.shields.io/npm/v/three-quickvr.svg
+[npm-badge-url]: https://www.npmjs.com/package/three-quickvr
+[license-badge]: https://img.shields.io/npm/l/three-quickvr.svg
+[license-badge-url]: ./LICENSE.md
+[dependencies-badge]: https://img.shields.io/david/halvves/three-quickvr.svg
+[dependencies-badge-url]: https://david-dm.org/halvves/three-quickvr
---
+++ src/quickvr.js
@@ -0,0 +1,138 @@
+window.WebVRConfig = {BUFFER_SCALE: 1.0};
+
+import 'webvr-polyfill/build/webvr-polyfill';
+
+import {
+  Color,
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+} from 'three';
+
+import VRControls from 'three-vrcontrols-module';
+import VREffect from 'three-vreffect-module';
+
+import * as WebvrUI from 'webvr-ui/build/webvr-ui';
+
+export default class App {
+  constructor() {
+    const UI = document.createElement('div');
+    UI.setAttribute('id', 'ui');
+    UI.style.position = 'fixed';
+    UI.style.bottom = '10px';
+    UI.style.left = '50%';
+    UI.style.transform = 'translate(-50%, -50%)';
+    UI.style.textAlign = 'center';
+    UI.style.fontFamily = 'sans-serif';
+    UI.style.zIndex = '10';
+
+    const VR_BTN = document.createElement('div');
+    VR_BTN.setAttribute('id', 'vr-button');
+
+    const MAGIC_WIN = document.createElement('a');
+    MAGIC_WIN.setAttribute('id', 'magic-window');
+    MAGIC_WIN.setAttribute('href', '#');
+    MAGIC_WIN.style.display = 'block';
+    MAGIC_WIN.style.color = 'white';
+    MAGIC_WIN.style.marginTop = '1em';
+
+    const MAGIC_TXT = document.createTextNode('Try it without a headset');
+
+    MAGIC_WIN.appendChild(MAGIC_TXT);
+    UI.appendChild(VR_BTN);
+    UI.appendChild(MAGIC_WIN);
+    document.body.appendChild(UI);
+
+    this.animations = [];
+    this.backgroundColor = 0x000000;
+    this.vrDisplay;
+
+    this.uiOptions = {
+      color: '#e0e0e0',
+      corners: 'round',
+    };
+
+    const renderer = this.renderer = new WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    document.body.appendChild(renderer.domElement);
+
+    const effect = this.effect = new VREffect(renderer);
+    effect.setSize(window.innerWidth, window.innerHeight);
+
+    const scene = this.scene = new Scene();
+    scene.background = new Color(this.backgroundColor);
+
+    const camera = this.camera = new PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.set(0, 0, 0);
+
+    const controls = this.controls = new VRControls(camera);
+    controls.standing = true;
+    camera.position.y = controls.userHeight;
+
+    const vrButton = this.vrButton = new WebvrUI.EnterVRButton(renderer.domElement, this.uiOptions);
+    vrButton.on('exit', () => {
+      camera.quaternion.set(0, 0, 0, 1);
+      camera.position.set(0, controls.userHeight, 0);
+    });
+    vrButton.on('hide', function() {
+      document.getElementById('ui').style.display = 'none';
+    });
+    vrButton.on('show', function() {
+      document.getElementById('ui').style.display = 'inherit';
+    });
+    VR_BTN.appendChild(vrButton.domElement);
+    MAGIC_WIN.addEventListener('click', function() {
+      vrButton.requestEnterFullscreen();
+    });
+
+    navigator.getVRDisplays().then((displays) => {
+      if (displays.length > 0) {
+        this.vrDisplay = displays[0];
+        this.vrDisplay.requestAnimationFrame(this.render);
+      }
+    });
+
+    this._bind('render', 'resize');
+
+    window.addEventListener('resize', this.resize, true);
+    window.addEventListener('vrdisplaypresentchange', this.resize, true);
+  }
+
+  _bind(...methods) {
+    methods.forEach((method) => this[method] = this[method].bind(this));
+  }
+
+  render() {
+    this.effect.render(this.scene, this.camera);
+
+    this.animations.forEach((animation, i, a) => {
+      animation();
+    });
+
+    this.vrDisplay.requestAnimationFrame(this.render);
+
+    if (this.vrButton.isPresenting()) {
+      this.controls.update();
+    }
+  }
+
+  resize(e) {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.effect.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  add(asset) {
+    this.scene.add(asset);
+  }
+
+  addAnimation(animation) {
+    this.animations.push(animation);
+  }
+};
